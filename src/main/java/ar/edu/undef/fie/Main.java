da6c5e3702290189baa4a0c09fe23a62bc8344f8
“julioleonel” <jsendra@fie.undef.edu.ar>
2023-09-10 18:35:01 -0300

    subo codigo

--- src/main/java/ar/edu/undef/fie/Main.java
+++ src/main/java/ar/edu/undef/fie/Main.java
@@ -1,7 +1,30 @@
 package ar.edu.undef.fie;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese un número para calcular su factorial: ");
+
+        var numero = scanner.nextInt();
+
+        scanner.close(); // Importante cerrar el scanner
+
+        var factorial = 1; //  factorial a 1 como valor base
+
+        for (var i = 2; i <= numero; i++) {
+            factorial *= i;
+        }
+
+        System.out.println("El factorial de " + numero + " es " + factorial);
     }
 }
+
+
+
+/*
+Calcula el factorial de un número pedido por teclado
+ */
